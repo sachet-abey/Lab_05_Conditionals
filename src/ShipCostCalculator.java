c65db68a254fc8e75ee8e7cf79b6eb27bc4866a9
sachet-abey <155912536+sachet-abey@users.noreply.github.com>
2024-02-10 10:53:46 -0500

    Update ShipCostCalculator.java

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ShipCostCalculator {
+import java.util.Scanner;
+
+public class ShipCostCalculator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the price of an item: ");
+        double shippingCosts;
+        if(scan.hasNextDouble()) {
+            double itemPrice = scan.nextDouble();
+            if(itemPrice >= 100) {
+                shippingCosts = 0.00;
+            }
+            else {
+                shippingCosts = itemPrice * 2/100;
+            }
+            double totalPrice = itemPrice + shippingCosts;
+            System.out.println("The shipping costs is $"+shippingCosts+" and the total price is $"+totalPrice);
+        }
+    }
 }
