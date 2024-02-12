e9650430fcf21472bf252cae26d6b1a5630c2253
sachet-abey <155912536+sachet-abey@users.noreply.github.com>
2024-02-11 20:49:07 -0500

    Update ShipCostCalculator.java

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,21 +1,21 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import Scanner from Java
 
 public class ShipCostCalculator {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the price of an item: ");
-        double shippingCosts;
-        if(scan.hasNextDouble()) {
-            double itemPrice = scan.nextDouble();
-            if(itemPrice >= 100) {
+        Scanner scan = new Scanner(System.in); // Initialize a scanner to read user input
+        System.out.println("Enter the price of an item: "); // Prompts user to enter price of an item
+        double shippingCosts; // Initialize shipping costs variable
+        if(scan.hasNextDouble()) { // If the next input can be read as a double
+            double itemPrice = scan.nextDouble(); // Get item price input
+            if(itemPrice >= 100) { // If item price is greater than or equal to 100, shipping costs are free
                 shippingCosts = 0.00;
             }
-            else {
+            else { // Otherwise, the shipping costs is 2% of the item price (multiply item price by 2/100)
                 shippingCosts = itemPrice * 2/100;
             }
-            double totalPrice = itemPrice + shippingCosts;
-            System.out.println("The shipping costs is $"+shippingCosts+" and the total price is $"+totalPrice);
+            double totalPrice = itemPrice + shippingCosts; // The total price is calculated by adding the item price and the shipping costs
+            System.out.println("The shipping costs is $"+shippingCosts+" and the total price is $"+totalPrice); // Return results for shipping costs and total price
         }
     }
 }
