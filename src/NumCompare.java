f611270808f14f50f4bff12044140d8d9d3fddbc
sachet-abey <155912536+sachet-abey@users.noreply.github.com>
2024-02-11 20:50:26 -0500

    Update NumCompare.java

--- src/NumCompare.java
+++ src/NumCompare.java
@@ -1,30 +1,30 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import Scanner from Java
 public class NumCompare {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        double firstNum;
-        double secondNum;
-        System.out.println("Enter the first number: ");
-        if(scan.hasNextInt()) {
-            firstNum = scan.nextInt();
-            System.out.println("Enter the second number: ");
-            if(scan.hasNextInt()) {
-                secondNum = scan.nextInt();
-                if(firstNum < secondNum) {
-                    System.out.println("The first number you entered "+firstNum+" is the smaller number.");
+        Scanner scan = new Scanner(System.in); // Initialize a scanner to read user input
+        double firstNum; // Initialize the first number
+        double secondNum; // Initialize the second number
+        System.out.println("Enter the first number: "); // Prompts user to enter the first number
+        if(scan.hasNextInt()) { // If the next input can be read as an integer
+            firstNum = scan.nextInt(); // Gets input for first number
+            System.out.println("Enter the second number: "); // Prompts user to enter the second number
+            if(scan.hasNextInt()) { // Nested if statement to check if the next input can be read as an integer
+                secondNum = scan.nextInt(); // Gets input for second number
+                if(firstNum < secondNum) { // If first number is less than second number
+                    System.out.println("The first number you entered "+firstNum+" is the smaller number."); // Returns output saying that the first number is the smaller number
                 }
-                else if(secondNum < firstNum) {
-                    System.out.println("The second number you entered "+secondNum+" is the smaller number.");
+                else if(secondNum < firstNum) { // If second number is less than the first number
+                    System.out.println("The second number you entered "+secondNum+" is the smaller number."); // Returns output saying that the second number is the smaller number
                 }
-                else {
-                    System.out.println("The two numbers you entered are equal.");
+                else { // If no number is less than the other (the numbers must be equal)
+                    System.out.println("The two numbers you entered are equal."); // Returns output saying that the numbers are equal
                 }
             }
-            else {
+            else { // If the next input cannot be read as an integer
                 System.out.println("Please try again!");
             }
         }
-        else {
+        else { // If the next input cannot be read as an integer
             System.out.println("Please try again!");
         }
     }
