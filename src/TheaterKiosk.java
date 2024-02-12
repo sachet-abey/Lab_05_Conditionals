434f54dbceb2054048882ef5330c8a359db3ce31
sachet-abey <155912536+sachet-abey@users.noreply.github.com>
2024-02-11 20:49:59 -0500

    Update TheaterKiosk.java

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import Scanner from Java
 public class TheaterKiosk {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your age: ");
-        if(scan.hasNextInt()) {
-            int age = scan.nextInt();
-            if(age >= 21) {
-                System.out.println("You get a paper wrist band!");
+        Scanner scan = new Scanner(System.in); // Initialize a scanner to read user input
+        System.out.println("Enter your age: "); // Prompts user to enter their age
+        if(scan.hasNextInt()) { // If the next input can be read as an integer
+            int age = scan.nextInt(); // Gets input for age
+            if(age >= 21) { // If age is greater than or equal to 21 (age restriction to enter the theater)
+                System.out.println("You get a paper wrist band!"); // Returns output saying the user gets a paper wrist band
             }
         }
     }
